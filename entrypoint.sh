c9622f86528030f56d8df906e88f0c9a1946f78f
Mike Dodge <mike@lonewolfworks.com>
2022-01-09 16:17:48 -0500

    args vs env vars

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,10 @@
 printenv
 
 java -jar wolkezeug.jar ecs-push \
-			-e $DEPLOY_ENV \
-		    -t $TIMEOUT \
-    		-v bamboo.maven.artifactId=$ARTIFACT \
-    		-v bamboo.maven.version=$VERSION \
-    		-v aws.region=$REGION \
+			-e $1 \
+		    -t $3 \
+    		-v bamboo.maven.artifactId=$4 \
+    		-v bamboo.maven.version=$5 \
+    		-v aws.region=$2 \
     		-v bamboo.planRepository.revision=$PLANREVISION \
-    		-v bamboo.deploy.version=$VERSION
+    		-v bamboo.deploy.version=$5

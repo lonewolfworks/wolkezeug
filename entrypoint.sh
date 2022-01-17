f44870238c518019ebe3dfb452e405f1fb39bc2d
Mike Dodge <mike@lonewolfworks.com>
2022-01-17 17:04:39 -0500

    cleanup

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,6 @@ ls -alh
 java -jar /wolkezeug.jar ecs-push \
 			-e $1 \
 		    -t $3 \
-    		-v bamboo.maven.artifactId=$4 \
-    		-v bamboo.maven.version=$5 \
-    		-v aws.region=$2 \
-    		-v bamboo.planRepository.revision=$PLANREVISION \
-    		-v bamboo.deploy.version=$5
+    		-v app.artifactId=$4 \
+    		-v app.version=$5 \
+    		-v aws.region=$2 
